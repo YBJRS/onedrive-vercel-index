3c6832a43334dec9b0b5f465af1e14f79aee0dd8
YBJR <ybjrs@outlook.com>
2025-09-27 10:41:52 +0800

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,25 @@
+// const { i18n } = require('./next-i18next.config')
+
+// module.exports = {
+//   i18n,
+//   reactStrictMode: true,
+//   // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
+//   trailingSlash: true
+// }
+
 const { i18n } = require('./next-i18next.config')
 
 module.exports = {
   i18n,
   reactStrictMode: true,
-  // Required by Next i18n with API routes, otherwise API routes 404 when fetching without trailing slash
-  trailingSlash: true
+  trailingSlash: true,
+  webpack(config, { isServer }) {
+    if (isServer) {
+      // 如果是在服务器端构建，排除 plyr.js
+      config.externals = config.externals || [];
+      config.externals.push('plyr');  // 排除 plyr.js 让它只在客户端加载
+    }
+
+    return config;
+  }
 }
